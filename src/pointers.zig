007fdefd5144c0447f6769b87ed146b33651e7ab
SG <133551974+savleeng@users.noreply.github.com>
2024-04-04 04:16:00 -0500

    Pointer to an array; sentinel terminated pointers
    pointer to an array
      array size information in the pointer
      len method
    
    sentinel terminated pointer
      array size determined by the position of the sentinel value
      slice operation on an array

--- src/pointers.zig
+++ src/pointers.zig
@@ -53,14 +53,12 @@ fn multiItemPointer() void {
     arr_ptr[1] += 5; // `arr` second element value is now 7.
     std.debug.print("arr_ptr[1] += 5: arr_ptr[1]: {}, type: {}\n", .{ arr_ptr[1], @TypeOf(arr_ptr) });
 
-    // Pointer arithmetic
-    // NOTE: By incrementing the memory address pointer that
-    // points to the first u8 element of `arr`, we are not
-    // adding 1, per se. We are moving the pointer forward in
-    // memory by a size of ONE (1) u8. And, since arrays are
-    // contiguous blocks of memory, moving the pointer forward
-    // by u8 effectively results in a pointer that's now
-    // pointing at the next element in the array.
+    // Pointer Arithmetic
+    // By incrementing `arr_ptr`, we are not adding 1. `arr_ptr` is
+    // moved forward in memory by a size of one u8. Since arrays are
+    // contiguous blocks of memory, moving the pointer forward by u8
+    // results in `arr_ptr` pointing at the next element in `arr`.
+
     arr_ptr += 1;
 
     // The first element of the pointer is no longer pointing
@@ -75,11 +73,70 @@ fn multiItemPointer() void {
     std.debug.print("arr: {any}\n", .{arr});
 }
 
+fn pointerToArray() void {
+
+    // A pointer to an array refers to the entire array, not just the
+    // first element of the array. A pointer to an array carries size
+    // information about the array. This allows for checking the length
+    // of a given array. Provides type safety for out-of-bound access.
+
+    var arr = [_]u8{ 1, 2, 3, 4, 5 };
+    const arr_ptr = &arr;
+    std.debug.print("arr: {any}\n", .{arr});
+
+    std.debug.print("arr_ptr[0]: {}, type: {} ", .{ arr_ptr[0], @TypeOf(arr_ptr) });
+    std.debug.print("<- `arr` size known by `arr_ptr`\n", .{});
+
+    // Indexing to access arr element
+    arr_ptr[1] += 8;
+    std.debug.print("arr_ptr[1] += 8: arr_ptr[1]: {}, arr: {any}\n", .{ arr_ptr[1], arr });
+
+    // Accessing arr length because arr_ptr stores arr's size info
+    std.debug.print("arr_ptr.len: {}\n", .{arr_ptr.len});
+}
+
+fn sentinelTerminatedPointer() void {
+
+    // Sentinel Terminated Pointer
+    // [*:x]T, where `x` is the sentinel value.
+    // The length of a sentinel pointer is determined by the sentinel value.
+    // Protection against buffer overflows and overreads.
+    // https://ziglang.org/documentation/0.11.0/#Sentinel-Terminated-Pointers
+
+    var arr = [_]u8{ 1, 2, 3, 4, 5, 6 };
+    std.debug.print("arr: {any}\n", .{arr});
+
+    // Defining sentinel value at index 4
+    arr[4] = 0;
+
+    // Assignment using slice syntax [start..stop :sentinel_value]
+    const arr_ptr: [*:0]const u8 = arr[1..4 :0];
+    std.debug.print("arr[1..4 :0]: arr_ptr[0]: {}, type: {}\n", .{ arr_ptr[0], @TypeOf(arr_ptr) });
+
+    std.debug.print("arr: {any}\n\n", .{arr});
+
+    // @intFromPtr
+    const addr = @intFromPtr(arr_ptr);
+    std.debug.print("@intFromPtr: addr: {}, type: {}\n", .{ addr, @TypeOf(addr) });
+
+    // @ptrFromInt
+    const addr_ptr: [*:0]const u8 = @ptrFromInt(addr);
+    std.debug.print("@ptrFromInt: addr_ptr[1]: {}, type: {}\n", .{ addr_ptr[1], @TypeOf(addr_ptr) });
+}
+
 pub fn main() void {
     std.debug.print("==Single Item Pointer==\n", .{});
     singleItemPointer();
+
     std.debug.print("\n==Pointer Assignment==\n", .{});
     pointerAssignment();
+
     std.debug.print("\n==Multi-Item Pointer==\n", .{});
     multiItemPointer();
+
+    std.debug.print("\n==Pointer to an Array==\n", .{});
+    pointerToArray();
+
+    std.debug.print("\n==Sentinel-Terminated Pointer==\n", .{});
+    sentinelTerminatedPointer();
 }
